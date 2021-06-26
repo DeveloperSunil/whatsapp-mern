5d0321029d64d8e83bc8e3319e9e4c761e45ca91
sunil patel <sunil.patel.sp580@gmail.com>
2021-06-26 14:23:43 +0530

    Whatsapp-mern

---
+++ src/Chat.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import {useState} from 'react';
+import "./Chat.css";
+import AttachFile from '@material-ui/icons/AttachFileOutlined';
+import MoreVert from '@material-ui/icons/MoreVert';
+import SearchOutlined from '@material-ui/icons/SearchOutlined';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticonOutlined';
+import MicIcon from "@material-ui/icons/Mic";
+import {Avatar,IconButton} from '@material-ui/core';
+import axios from './axios';
+function Chat ({ messages }){
+    const[input,setInput] = useState("");
+    const sendMessage = (e) =>{
+        e.preventDefault();
+        axios.post('/messages/new',{
+            message:input,
+            name:"Demo App",
+            timestamp:"Just now!",
+            received:false
+        });
+        setInput("");
+    }
+    return (
+        <div className="chat">
+              <div className="chat_header">
+                    <Avatar />
+                    
+                    <div className="chat_headerInfo">
+                        <h3>Room name</h3>
+                        <p>Last seen at...</p>
+                    </div>   
+
+                    <div className="chat_headerRight">
+                            <IconButton>
+                                <SearchOutlined /> 
+                             </IconButton>
+                        
+                            <IconButton>
+                                <AttachFile/>
+                            </IconButton>
+
+                            <IconButton>
+                                <MoreVert />
+                            </IconButton>
+                    </div>
+              </div>
+
+              <div className="chat_body">
+                  {messages.map(message =>(
+                     <p className={` chat_message ${message.received && "chat_receiver"} `}> 
+                        <span className="chat_name">{message.name}</span>
+                         {message.message}
+                        <span className="chat_timestamp">
+                            {message.timestamp}
+                        </span>
+                    </p> 
+                  ))}
+                    
+
+                    {/* <p className="chat_message chat_receiver"> 
+                        <span className="chat_name">Sonny</span>
+                        This is a message
+                        <span className="chat_timestamp">
+                            {new Date().toUTCString()}
+                        </span>
+                    </p>  */}
+              </div> 
+              
+              <div className="chat_footer">
+                        <InsertEmoticonIcon />
+                    <form>
+                        <input value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Type a message" type="text" />
+                        <button onClick={sendMessage} type="submit">Send a message</button>
+                    </form>
+                    <MicIcon />
+              </div>
+
+        </div>
+    );
+}
+export default Chat;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAfUuvA1p-jUwFq5zpHgfSRhGdGO7gfn2Y",
+    authDomain: "whatsapp-mern-e3f01.firebaseapp.com",
+    projectId: "whatsapp-mern-e3f01",
+    storageBucket: "whatsapp-mern-e3f01.appspot.com",
+    messagingSenderId: "671165452706",
+    appId: "1:671165452706:web:c4abfd2f609340be79e2fb"
+  };
